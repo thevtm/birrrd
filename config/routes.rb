a39f76526f513d06f6a9243d91b0519eb36338b9
Young Lee <younghoonlee1205@gmail.com>
2018-06-12 15:27:34 +0200

    feat: create show user page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :users, only: [:show]
+
   resources :birds, only: [:index, :new, :create, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
